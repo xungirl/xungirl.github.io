963d31fa5bf6edbe82d964549a2a0a0a4ef867c4
xun-girl <xungirl1024@gmail.com>
2024-04-15 23:41:28 +0800

    api

--- static/js/travel/weather-nanjing.js
+++ static/js/travel/weather-nanjing.js
@@ -1,7 +1,7 @@
 const API_KEY_NanJing = "13014273332be0f221173b22cb4db50d";
 const CITY = "Nanjing";
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=${API_KEY}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=${API_KEY_NanJing}`)
 .then(response => response.json())
 .then(data => {
     document.getElementById("temperature-nanjing").textContent = data.main.temp;
